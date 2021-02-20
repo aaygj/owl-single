6a4a9f3e5560644d3759cd049372d080ee555999
jingfeng <jingfengcom@163.com>
2021-02-20 16:15:52 +0800

    初始化代码

---
+++ owl-manage/src/main/java/com/owlsingle/manage/OwlManageApplication.java
@@ -0,0 +1,13 @@
+package com.owlsingle.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlManageApplication.class, args);
+	}
+
+}
